b2583cf193588143ecd8301806b5a06f257e6a84
aleem-dev <96736085+aleem-dev@users.noreply.github.com>
2024-02-02 22:33:56 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountID = 12234;
+let accountEmail = 'abc@abc.com';
+var accountPassword = '212121';
+accountCity = 'Vancouver';
+let accountProvence;
+
+// accountID = 45454;
+
+console.log(accountID);
+console.table([accountID,accountEmail,accountPassword,accountCity,accountProvence]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+let name = 'John';
+let age = 18
+let isUserLoggedIn = false
+
+console.log(typeof name);
+console.log(typeof age);
+
+// types of data types
+// Premetive
+// string
+// number
+// bigInt
+// boolean
+// symbol //symbol is used extensively in react to define uniqness of component in large web application
+
+// object
+
+// null // is a stand alone value, is a type object -> it is sometimes called language error
+// undefined    //is a type similar to string or number
+
+let temprature = null;
+let provence;
+
+console.log(typeof temprature);
+console.log(typeof provence);
