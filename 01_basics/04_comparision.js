ec2e267a8f5e8ccb8864be4c31f816a4b1a93ec3
aleem-dev <96736085+aleem-dev@users.noreply.github.com>
2024-02-05 23:13:36 +0000

    comparision done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,15 @@
+// console.log(2>1)     // => true
+// console.log(2!=1)    // => ture
+
+// console.log("2" <1)  // => false
+
+// console.log(null > 0);  //  => false
+// console.log(null == 0); //  => false
+// console.log(null >= 0); //  => true, as in above line equility check converted the null to 0, that is the reason the output is true
+
+// strick check === here we only check same datatype, wherein == we compare any datatype
+
+console.log(1 === 1); // => true
+console.log("1" === 1); // => false, it did not gave error, it gave false
+
+
