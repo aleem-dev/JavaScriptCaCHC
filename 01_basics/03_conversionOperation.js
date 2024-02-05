a612fec66b588370e21fb39e325d03f769fcd25f
aleem-dev <96736085+aleem-dev@users.noreply.github.com>
2024-02-05 22:51:30 +0000

    Operation arithmetic and postfix, prefix

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,3 +22,30 @@ let booleanisLogIn = Boolean(isLogIn)
 // string number => true and type of true is boolean
 // True => 1, False => 0, undefined => false and type of false is is boolean
 
+// Conversion //
+
+let value = 1
+let nagValue = -value
+
+// console.log(nagValue);
+
+// console.log(1 + 1);
+// console.log("Hello" + " Aleem");
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "3");
+
+let gameCounter = 100
+gameCounter++   // post fix increament increases the value first by one and then do the operation, 
+                //while post fix do the operation then increases the value by one
+console.log(gameCounter);
+
+// Reference Reading
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+// https://tc39.es/ecma262/multipage/ecmascript-language-expressions.html#sec-postfix-increment-operator
+
+
+
+
+
