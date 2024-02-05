8cb5ca16870028b2708a75dbb84f6f4bfe99cf72
aleem-dev <96736085+aleem-dev@users.noreply.github.com>
2024-02-05 22:18:44 +0000

    Conversion Number, Boolean

---
+++ 01_basics/03_conversionDatatypes.js
@@ -0,0 +1,24 @@
+let score = "abc" // change value here and uncomment the consle log
+
+let numberScore = Number (score)
+
+// console.log(typeof score);
+// console.log(numberScore);
+// console.log(typeof numberScore);
+
+// Number () is a function helps to convert the input to number
+// string non numbers or text => NaN and type of NaN is number
+// string number => number
+// True => 1, False => 0, undefined => NaN and type of NaN is number
+
+let isLogIn = "abc"  // change value here
+let booleanisLogIn = Boolean(isLogIn)
+
+// console.log(booleanisLogIn);
+// console.log(typeof booleanisLogIn);
+
+// Boolean () is a function helps to convert the input to boolean value
+// string non numbers or text => true and type of true is boolean
+// string number => true and type of true is boolean
+// True => 1, False => 0, undefined => false and type of false is is boolean
+
