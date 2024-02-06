73fff07b928f9546677e7ee283efc10d6357f1b6
aleem-dev <96736085+aleem-dev@users.noreply.github.com>
2024-02-06 20:11:25 +0000

    stringmethod file created

---
+++ 01_basics/06_stringMethods.js
@@ -0,0 +1,11 @@
+let gameName = "AleemGT"
+console.log(gameName);
+console.log(gameName.charAt(2));
+console.log(gameName.charCodeAt(2));
+console.log(gameName.codePointAt(2));
+console.log(gameName.concat("Extream"));
+console.log(gameName.endsWith("GT",7));
+console.log(gameName.includes("T"));
+console.log(gameName.indexOf("e"));
+console.log(gameName.lastIndexOf("e"));
+console.log(gameName.length);
