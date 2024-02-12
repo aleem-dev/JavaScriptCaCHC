708b3ddb99733158c05a8fda2fa1fddf224add30
aleem-dev <96736085+aleem-dev@users.noreply.github.com>
2024-02-12 21:42:20 +0000

    dateJS file name change, datetime project show difference between two timezones

---
+++ 01_basics/08_dateJS.js
@@ -0,0 +1,24 @@
+let myDate = Date.now();
+console.log(`date.now() output is number type and miliseconds: ${typeof myDate}`);
+
+let newDate = new Date();
+console.log(`instantiated with new Date() class output is full date: ${typeof newDate}`);
+// console.log(newDate.getDate());
+// console.log(newDate.getDay());
+// console.log(newDate.getFullYear());
+// console.log(newDate.getHours());
+// console.log(newDate.getMonth());
+// console.log(newDate.getTime());
+// console.log(newDate.getTimezoneOffset());
+// console.log(newDate.getUTCDate());
+// console.log(newDate.toDateString())
+// console.log(newDate.toISOString());
+// console.log("toJSON: " + newDate.toJSON());
+// console.log(newDate.toLocaleDateString());
+// console.log(newDate.toLocaleString());
+// console.log(newDate.toString());
+// console.log(newDate.toUTCString());
+
+
+let toDate = Date();
+console.log(`instantiated Date() class output output is full date: ${typeof toDate}`);
