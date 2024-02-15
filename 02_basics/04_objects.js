618a27ab9093d65a01e21869ace01eb763799f84
aleem-dev <96736085+aleem-dev@users.noreply.github.com>
2024-02-15 19:48:38 +0000

    object destructure and json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -58,15 +58,60 @@ const users = [
 
 //pending video 12:55 https://youtu.be/4lb2pXWWXJI?feature=shared
 
-console.log(users[0].email);    // now when data come from database as in array of object we can use . operator
-                                // we can also use map or filter mathod to get the desired data
-console.log(tinderUsr);
-console.log(Object.keys(tinderUsr));    // ** very improtoant **  we get all the keys in an array, very usefull with database and projects
-console.log(Object.values(tinderUsr));
-console.log(Object.entries(tinderUsr)); // converts and object to array of array for each key value pair.  Note if there is and object withing the object it wont be able to covert it
+// console.log(users[0].email);    // now when data come from database as in array of object we can use . operator
+//                                 // we can also use map or filter mathod to get the desired data
+// console.log(tinderUsr);
+// console.log(Object.keys(tinderUsr));    // ** very improtoant **  we get all the keys in an array, very usefull with database and projects
+// console.log(Object.values(tinderUsr));
+// console.log(Object.entries(tinderUsr)); // converts and object to array of array for each key value pair.  Note if there is and object withing the object it wont be able to covert it
 
 
 // many times we loop an object or array and that time if the value does not exist the entire program crashes
 // solution is that use hasOwnProperty() method to check
 
-console.log(tinderUsr.hasOwnProperty("isLogedIn")); // true check the if the property is there or not
+// console.log(tinderUsr.hasOwnProperty("isLogedIn")); // true check the if the property is there or not
+
+
+
+
+// ** Destructing objects and array
+
+// object course
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "Aleem"
+}
+
+// we access the property with . operator however we use other method too that is called destruction {key: optinal short form for key}
+
+const {courseInstructor} = course
+// console.log(courseInstructor); // Aleem => to avoide writing long name we can use another syntax
+
+const {courseInstructor: instructor} = course;
+// console.log(instructor);    // Aleem
+
+// React example for destructor
+// const navbar = (props.company) => { // to avoid writing long syntax
+// const navbar = ({company}) => {     // we destructure it between {} curly braces
+
+// }
+
+// navbar(company = "Aleem")
+
+
+
+// ** API JSON object
+// keys and values are strings, every line ends with , and last line does not have , comma
+// below is json example
+// {
+//     "name": "Aleem",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+// how to study json object from api response?
+    // take a api response and read it to json readors on web
+    // many times we get api reponse in array of objects
+    // famous api random user me https://randomuser.me/
+    // take api response and paste it to json formator https://jsonformatter.org/
