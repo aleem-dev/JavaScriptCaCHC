9f45f0a231c3aa2cebe93bbfc8def7a6193a3607
aleem-dev <96736085+aleem-dev@users.noreply.github.com>
2024-03-08 19:38:15 +0000

    for...in iterates over enumerable properties of an object. The keys in a map are not enumerable properties (though a map, like all objects, can have enumerable properties). For one thing, they can be of any type, not just strings, as with JavaScript objects, which have historically stood in for maps.
    for...of iterates over an iterable, which is how the Map yields up its keys/values for iteration.

--- 05_iterations/04_for_in.js
+++ 05_iterations/04_for_in.js
@@ -1,3 +1,19 @@
+// for in is a loop to iterate through objects
+//this is a special loop for objects it also works on other datatypes
+// we can not use for in loop to iterate over map
+// we can not use for of loop to iterate over objects
+// for of arrays, maps
+// for in objects, arrays (for arrays, you need to use the key means the index value starting from 0, 1, 2..)
+// learn in depth why we can not use for in for map and for of for objects?
+/* 
+Ans:
+for...in iterates over enumerable properties of an object. 
+The keys in a map are not enumerable properties (though a map, like all objects, can have enumerable properties). 
+For one thing, they can be of any type, not just strings, as with JavaScript objects, which have historically stood in for maps.
+
+for...of iterates over an iterable, which is how the Map yields up its keys/values for iteration.
+*/
+
 const myObj = {
     js :"Javascript",
     cpp:"c++",
@@ -31,4 +47,4 @@ for (const key in map) {
     console.log(key);
 }
 
-// nothing happence as Map is not iterable
+// nothing happence as Map why?
