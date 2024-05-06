62014bced9f1cb9960c4ac03f243db04a8d20af6
aleem-dev <96736085+aleem-dev@users.noreply.github.com>
2024-05-06 18:28:23 +0000

    map is to create new array and reduce is to get total of shopping cart

---
+++ 05_iterations/07_map.js
@@ -0,0 +1,13 @@
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+
+// chaining
+const newNums = myNumers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNums);
+
+// note: unlike filter map returns the whole array , also from stackoverflow => The map method is used to convert each item of an array, while the filter method is used to select certain items of an array. 
---
+++ 05_iterations/08_reduce.js
@@ -0,0 +1,39 @@
+// real life usecase for reduce is to get the total of shopping cart
+
+// notes : we need to supply three things, accumalator, current value and initial value (0) to get the sum
+
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+
+console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+// item.price is current value
+
+console.log(priceToPay);
